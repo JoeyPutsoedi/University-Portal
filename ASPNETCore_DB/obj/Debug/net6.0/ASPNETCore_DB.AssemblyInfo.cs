aa12d7b80b9b51c2bba51cc9316a29ac247feb1f
Joey Putsoedi <joeyputsoedi6@gmail.com>
2025-11-14 06:30:59 +0200

    Add frontend dependencies and update project files
    Added npm packages and node_modules for frontend tooling (autoprefixer, browserslist, caniuse-lite, etc.) and updated ASPNETCore_DB project files, views, and database. This prepares the project for frontend asset management and improves compatibility with modern web standards.

--- ASPNETCore_DB/obj/Debug/net6.0/ASPNETCore_DB.AssemblyInfo.cs
+++ ASPNETCore_DB/obj/Debug/net6.0/ASPNETCore_DB.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ASPNETCore_DB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15ddc844669e726ec9519792aeb25f56104fa178")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa91059f8ed44d3b8c79b49c74086d0c5fc5caf8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ASPNETCore_DB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ASPNETCore_DB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
