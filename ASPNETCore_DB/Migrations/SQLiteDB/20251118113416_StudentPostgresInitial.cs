d3bc4b27be9a3e6cb6746bc61ac08ecaa3d37786
Joey Putsoedi <joeyputsoedi6@gmail.com>
2025-11-18 13:46:06 +0200

    preparing for railway

---
+++ ASPNETCore_DB/Migrations/SQLiteDB/20251118113416_StudentPostgresInitial.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ASPNETCore_DB.Migrations.SQLiteDB
+{
+    /// <inheritdoc />
+    public partial class StudentPostgresInitial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Student",
+                columns: table => new
+                {
+                    StudentNumber = table.Column<string>(type: "character varying(10)", maxLength: 10, nullable: false),
+                    FirstName = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Surname = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    EnrollmentDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Photo = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Student", x => x.StudentNumber);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Student");
+        }
+    }
+}
